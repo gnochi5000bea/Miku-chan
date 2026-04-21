4d63a7743fd490d5f01c717efa64605f5fde6368
cunning <76720185+gnochi5000bea@users.noreply.github.com>
2026-04-20 22:33:34 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 local REPO = "https://raw.githubusercontent.com/gnochi5000bea/Miku-chan/refs/heads/main/games/"
 local SCRIPTS = {
-    [18794863104] = "Demonology.lua",
+    [18794863104] = "Demonology",
 }
 
 local place_id = game.PlaceID
@@ -10,7 +10,7 @@ if not script_url then
     return
 end
 
-script_url = REPO .. script_url
+script_url = REPO .. script_url .. ".lua"
 
 http.Get(script_url, {}, function(response)
     if not response then
