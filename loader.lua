b563b274a1dc54221a06138c3de039ebb0c1f69a
cunning <76720185+gnochi5000bea@users.noreply.github.com>
2026-04-20 22:31:52 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,21 @@
+local REPO = "https://raw.githubusercontent.com/gnochi5000bea/Miku-chan/refs/heads/main/games/"
+local SCRIPTS = {
+    [18794863104] = "Demonology.lua",
+}
 
+local place_id = game.PlaceID
+local script_url = SCRIPTS[place_id]
+
+if not script_url then
+    return
+end
+
+script_url = REPO .. script_url
+
+http.Get(script_url, {}, function(response)
+    if not response then
+        return
+    end
+
+    cheat.LoadString(response, "loaded_script_" .. tostring(place_id))
+end)
